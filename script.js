3d56d7210fc8402509b943f92012c132aa4d13f1
Hakki <mohdhakki12@hotmail.com>
2021-11-02 19:08:01 +0200

    Added an alert for the number of characters needed Formated the document

--- script.js
+++ script.js
@@ -2,80 +2,81 @@
 var generateBtn = document.querySelector("#generate");
 var specific = "";
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function randomlowercase(){
-  return String.fromCharCode (Math.floor(Math.random() * 26) + 97);
-
+function randomlowercase() {
+  console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-
-
-function randomuppercase(){
-  return String.fromCharCode (Math.floor(Math.random() * 26) + 65);
-
+function randomuppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-
-function randomnumber(){
-  return String.fromCharCode (Math.floor(Math.random() * 10) + 48);
-
+function randomnumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function randomsymbol(){
-  var symbol = "`!@#$%^&*()_+=[]{}:?/.,<>"
-  return symbol [Math.floor(Math.random() * symbol.length)]
-
+function randomsymbol() {
+  var symbol = "`!@#$%^&*()_+=[]{}:?/.,<>";
+  return symbol[Math.floor(Math.random() * symbol.length)];
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  var result = "";
 
-function generatePassword(){
-  var result = ""
+  var passwordLength = prompt(
+    "Choose the number of characters you want in the password, choose from 8-128"
+  );
 
-  var length = prompt ("Choose the number of characters you want in the password, choose from 8-128")
+  const parsedPasswordLength = parseInt(passwordLength, 10);
+  console.log(passwordLength, parsedPasswordLength);
+  if (parsedPasswordLength < 8 || parsedPasswordLength > 128) {
+    alert("Please choose a number between 8 - 128");
+    return;
+  } else {
+    var hasrandomlowercase = confirm(
+      "Do you want to include lowercase letters?"
+    );
+    var hasrandomuppercase = confirm(
+      "Do you want to include uppercase letters?"
+    );
+    var hasrandomnumber = confirm("Do you want to include numbers?");
+    var hasrandomsymbol = confirm("Do you want to include special characters?");
 
-  var hasrandomlowercase = confirm("Do you want to include lowercase letters?");
-  var hasrandomuppercase = confirm("Do you want to include uppercase letters?");
-  var hasrandomnumber = confirm("Do you want to include numbers?");
-  var hasrandomsymbol = confirm("Do you want to include special characters?");
+    if (hasrandomlowercase) {
+      specific += randomlowercase();
+      console.log(hasrandomlowercase);
+    }
 
-  if(hasrandomlowercase){
-    specific += randomlowercase()
+    if (hasrandomuppercase) {
+      specific += randomuppercase();
+      console.log(hasrandomuppercase);
+    }
 
+    if (hasrandomnumber) {
+      specific += randomnumber();
+      console.log(hasrandomnumber);
+    }
+
+    if (hasrandomsymbol) {
+      specific += randomsymbol();
+      console.log(hasrandomsymbol);
+    }
+
+    for (var i = 0; i < length; i++) {
+      result += specific.charAt(Math.floor(Math.random() * specific.length));
+      console.log(result);
+    }
   }
-  if(hasrandomuppercase){
-    specific += randomuppercase()
-
-  }
-  if(hasrandomnumber){
-    specific += randomnumber()
-
-  }
-  if(hasrandomsymbol){
-   specific += randomsymbol()
-
-  }
-
-  for (var i = 0; i < length; i++){
-    result += specific;
-  }
-
-  return result
+  return result;
 }
-
-
-
-
-
